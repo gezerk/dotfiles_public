5fecaff70f3e66b66ba63eac9c5c8eb6f53006b2
George Simpson <gsimpson@netflix.com>
2018-12-14 11:15:36 -0800

    Add new aliases

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -10,4 +10,12 @@ alias cd..="cd .."
 # sort top by CPU by default
 alias top="top -o cpu"
 
-alias tf="tail -f"
+alias tf="colourify tail -f"
+
+alias psg="colourify ps aux | grep -v grep | grep -i -e VSZ -e"
+
+alias df="colourify df -ha"
+
+alias ps="colourify ps aux"
+
+alias psg="colourify ps aux | grep -v grep | grep -i -e VSZ -e"
