4e79d07d02d351c5712fd557061956d764814385
George Simpson <gsimpson@netflix.com>
2019-01-28 08:42:26 -0800

    Missed some merge <<< markers

--- system/functions.zsh
+++ system/functions.zsh
@@ -1,10 +1,8 @@
-<<<<<<< HEAD
 function aliasSearch() {
     alias | grep $1
 }
-=======
-mcd () {
+
+function mcd () {
     mkdir -p $1
     cd $1
 }
->>>>>>> cbc049a4a184abb8e73405b1c1468438c0017970
