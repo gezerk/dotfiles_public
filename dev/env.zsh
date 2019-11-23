b0b066ccd8bc7c22d39f6479917b388269314015
George Simpson <gsimpson@netflix.com>
2019-11-22 20:41:44 -0800

    wish me luck squidward

--- dev/env.zsh
+++ dev/env.zsh
@@ -1,2 +1,6 @@
 #export ANDROID_HOME=/usr/local/opt/android-sdk
 #export PATH=$PATH:$HOME/.cargo/bin
+
+# Get you some of this Squidman!
+export http_proxy='http://localhost:6969'    
+export https_proxy='https://localhost:6969'
